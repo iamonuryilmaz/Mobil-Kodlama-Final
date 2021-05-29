d8c26eb372202f1a37cfb175e00ae6ec2af1aeb4
Onur Yılmaz <muh.onuryilmaz@gmail.com>
2021-05-29 15:55:47 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/gedik/mobil_kodlama_final/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gedik.mobil_kodlama_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+      },
+      theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.orange),
+    ),
+  );
+}
